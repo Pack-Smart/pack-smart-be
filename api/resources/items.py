0d080cf0aa93c9f3a8d8b37b1179310164958b6a
Kevin Cuadros <kev040798@gmail.com>
2021-02-20 20:22:22 -0500

    Created first end-point.
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Sheryl Stillman <sheryl.stillman@gmail.com>
    Co-authored-by: Roberto Basulto <Roberto.Basulto085@gmail.com>

--- api/resources/items.py
+++ api/resources/items.py
@@ -2,62 +2,57 @@ import datetime
 import json
 # import code; code.interact(local=dict(globals(), **locals()))
 
-import flask
-import bleach
-
+from flask import Flask
 from flask import request, jsonify
 from flask_restful import Resource, abort
-from sqlalchemy.orm.exc import NoResultFound
 from api import db
 from api.database.models import Item
 
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pack_smart_dev'
+db = SQLAlchemy(app)
 
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
-# books = [
-#     {'id': 0,
-#      'title': 'A Fire Upon the Deep',
-#      'author': 'Vernor Vinge',
-#      'first_sentence': 'The coldsleep itself was dreamless.',
-#      'year_published': '1992'},
-#     {'id': 1,
-#      'title': 'The Ones Who Walk Away From Omelas',
-#      'author': 'Ursula K. Le Guin',
-#      'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-#      'published': '1973'},
-#     {'id': 2,
-#      'title': 'Dhalgren',
-#      'author': 'Samuel R. Delany',
-#      'first_sentence': 'to wound the autumnal city.',
-#      'published': '1975'}
-# ]
+# app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
+
+def _item_payload(item):
+  return {
+    "name": item.name,
+    "quantity": 0,
+    "is_checked": False
+  }
 
 @app.route('/', methods=['GET'])
 def home():
-    import pdb; pdb.set_trace()
-    return Item.session.query.all()
+  items = db.session.query(Item).all()
+  categories = {}
+
+  for item in items:
+    # import pdb; pdb.set_trace()
+    if item.category in categories:
+      categories[item.category].append(_item_payload(item))
+    else:
+      categories[item.category] = []
+      categories[item.category].append(_item_payload(item))
 
 
-# @app.route('/api/v1/resources/books/all', methods=['GET'])
-# def api_all():
-#     return jsonify(books)
+  category_obj = {
+    "data": {
+      "id": 0,
+      "type": "Survey_Results",
+      "attributes": {
+        "categories": categories
+      }
+    }
+  }
+
+  return jsonify(category_obj)
 
 app.run()
 
-# def _user_payload(user):
-#     return {
-#         'id': user.id,
-#         'username': user.username,
-#         'email': user.email,
-#         'links': {
-#             'get': f'/api/v1/users/{user.id}',
-#             'patch': f'/api/v1/users/{user.id}',
-#             'delete': f'/api/v1/users/{user.id}',
-#             'index': '/api/v1/users',
-#         }
-#     }
 
 
 # @app.route('/api/v1/lists/new', methods=['GET'])
