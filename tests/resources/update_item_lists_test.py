fed77e186263738623b170e1abf999878d9c96ce
Roberto Basulto <roberto.basulto085@gmail.com>
2021-03-02 16:05:31 -0500

    Created tests for agecases with new patch method
    Co-authored-by: Jose Lopez <joselopez11394@gmail.com>

--- tests/resources/update_item_lists_test.py
+++ tests/resources/update_item_lists_test.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 from copy import deepcopy
-from api.database.models import PackingLists, Users, Item, ItemLists
+from api.database.models import PackingLists, Users, Item, ItemLists, CustomItem
 
 from api import create_app, db
 from tests import db_drop_everything, assert_payload_field_type_value, \
@@ -33,30 +33,59 @@ class GetAllUserPackingLists(unittest.TestCase):
     self.item_5 = Item(category = 'Accessories', weather = 'ALl', gender = 'All', item = 'Belts')
     self.item_5.insert()
 
-    self.item_list_1 = ItemLists(packing_list_id = 1, item_id = self.item_1.id, quantity = 5, is_checked = False)
+    self.item_list_1 = ItemLists(packing_list_id = self.packing_list_1.id, item_id = self.item_1.id, quantity = 5, is_checked = False)
     self.item_list_1.insert()
 
-    self.item_list_2 = ItemLists(packing_list_id = 1, item_id = self.item_5.id, quantity = 50, is_checked = True)
+    self.item_list_2 = ItemLists(packing_list_id = self.packing_list_1.id, item_id = self.item_5.id, quantity = 50, is_checked = True)
     self.item_list_2.insert()
 
-    self.item_list_3 = ItemLists(packing_list_id = 1, item_id = self.item_2.id, quantity = 20, is_checked = True)
+    self.item_list_3 = ItemLists(packing_list_id = self.packing_list_1.id, item_id = self.item_2.id, quantity = 20, is_checked = True)
     self.item_list_3.insert()
 
-    self.payload = {
-        "data": {
-            "items": [
-                {
-                    "id": self.item_list_1.id,
-                    "is_checked": True,
-                    "quantity": 16
-                },
-                {
-                    "id": self.item_list_2.id,
-                    "is_checked": False,
-                    "quantity": 25
-                }
-            ]
-        }
+    self.custom_item = CustomItem(item = 'Belt', quantity = 0, category = 'Accessories', is_checked = False, packing_list_id = self.packing_list_1.id)
+    self.custom_item.insert()
+
+    self.bulk_items_payload = {
+      "data": {
+        "item": [
+          {
+            "id": self.custom_item.id,
+            "is_checked": True,
+            "quantity": 32,
+            "category": "something"
+          },
+          {
+            "id": self.item_list_1.id,
+            "is_checked": True,
+            "quantity": 16
+          }
+        ]
+      }
+    }
+
+    self.single_item_payload_custom = {
+      "data": {
+        "item": [
+          {
+            "id": self.custom_item.id,
+            "is_checked": True,
+            "quantity": 32,
+            "category": "something"
+          }
+        ]
+      }
+    }
+
+    self.single_item_payload_item_list = {
+      "data": {
+        "item": [
+          {
+            "id": self.item_list_1.id,
+            "is_checked": True,
+            "quantity": 32
+          }
+        ]
+      }
     }
 
   def tearDown(self):
@@ -64,7 +93,7 @@ class GetAllUserPackingLists(unittest.TestCase):
     db_drop_everything(db)
     self.app_context.pop()
 
-  def test_it_can_update_item_lists(self):
+  def test_it_can_update_item_lists_and_custom_items_in_bulk(self):
 
     self.assertEqual(5, self.item_list_1.quantity)
     self.assertEqual(False, self.item_list_1.is_checked)
@@ -74,7 +103,7 @@ class GetAllUserPackingLists(unittest.TestCase):
 
     self.assertEqual(20, self.item_list_3.quantity)
     self.assertEqual(True, self.item_list_3.is_checked)
-    payload = deepcopy(self.payload)
+    payload = deepcopy(self.bulk_items_payload)
 
     response = self.client.patch(
       '/api/v1/item_list/update', json=(payload),
@@ -86,8 +115,42 @@ class GetAllUserPackingLists(unittest.TestCase):
     self.assertEqual(16, self.item_list_1.quantity)
     self.assertEqual(True, self.item_list_1.is_checked)
 
-    self.assertEqual(25, self.item_list_2.quantity)
-    self.assertEqual(False, self.item_list_2.is_checked)
+    self.assertEqual(50, self.item_list_2.quantity)
+    self.assertEqual(True, self.item_list_2.is_checked)
 
     self.assertEqual(20, self.item_list_3.quantity)
     self.assertEqual(True, self.item_list_3.is_checked)
+
+  def test_it_can_update_single_item_list(self):
+    
+    self.assertEqual(5, self.item_list_1.quantity)
+    self.assertEqual(False, self.item_list_1.is_checked)
+
+    payload = deepcopy(self.single_item_payload_item_list)
+    
+    response = self.client.patch(
+      '/api/v1/item_list/update', json=(payload),
+      content_type='application/json'
+    )
+
+    self.assertEqual(200, response.status_code)
+
+    self.assertEqual(32, self.item_list_1.quantity)
+    self.assertEqual(True, self.item_list_1.is_checked)
+
+  def test_it_can_update_single_custom_item(self):
+      
+    self.assertEqual(0, self.custom_item.quantity)
+    self.assertEqual(False, self.custom_item.is_checked)
+
+    payload = deepcopy(self.single_item_payload_custom)
+  
+    response = self.client.patch(
+      '/api/v1/item_list/update', json=(payload),
+      content_type='application/json'
+    )
+
+    self.assertEqual(200, response.status_code)
+
+    self.assertEqual(32, self.custom_item.quantity)
+    self.assertEqual(True, self.custom_item.is_checked)
